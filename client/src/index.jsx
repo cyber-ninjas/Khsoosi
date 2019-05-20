711d400a4daa68a0c4a24fa00b9eac00b379f46f
Ahmad-Al-Darkashli <alderkshli.ahmad@gmail.com>
2019-05-20 12:20:41 +0300

    imageUpload done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,7 @@ import Rating from "./components/Rating.jsx";
 import Search from './components/search.jsx';
 import ResultSearch from './components/resultSearch.jsx';
 import Header from './components/Header.jsx';
-
+import {storage} from '../../server/database/firebase.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -39,50 +39,30 @@ class App extends React.Component {
     if (e.target.files[0]) {
       const image = e.target.files[0];
       this.setState(() => ({image}));
-      console.log(image);
-    }
-    console.log(this.state);
-    
+    } 
   }
   handleImgUpload () {
-    // e.preventDefault();
-    const formData = new FormData();
-    formData.append('myImage',this.state.image);
-    // const body = this.state.image
-    fetch('/profileUpdate', {
-      method: 'post',
-      body: (formData)
-      // headers: {
-      //   'Accept': 'application/json',
-      //   "Content-Type": "multipart/form-data"
-      // }
-    })
-    // .then((res) => {
-    //   return res.json();
-    // }).then((data) => {
-    //   this.setState({imgUrl: data.url});
-    // });
-		// const {image} = this.state;
-		// const uploadTask = storage.ref(`images/${image.name}`).put(image);
-		// uploadTask.on('state_changed', 
-		// (snapshot) => {
-		// 	//progress function ....
-		// 	const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-		// 	this.setState({progress});
-		// },
-		// (error) => {
-		// 	// error function ....
-		// 	console.log(error);
-		// },
-		// () => {
-		// 	// complete function ....
-		// 	storage.ref('images').child(image.name).getDownloadURL().then(imgUrl => {
-    //     this.setState({imgUrl});
-    //     console.log(this.state);
-    //     console.log(imgUrl);
-    //   })
-		// });
-    // console.log(this.state);
+
+		const {image} = this.state;
+		const uploadTask = storage.ref(`images/${image.name}`).put(image);
+		uploadTask.on('state_changed', 
+		(snapshot) => {
+			//progress function ....
+			const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+			this.setState({progress});
+		},
+		(error) => {
+			// error function ....
+			console.log(error);
+		},
+		() => {
+			// complete function ....
+			storage.ref('images').child(image.name).getDownloadURL().then(imgUrl => {
+        this.setState({imgUrl});
+
+      })
+		});
+   
   }
   onchangingSignUp(e){
     this.setState({[e.target.name]:e.target.value});
@@ -102,14 +82,14 @@ class App extends React.Component {
       this.setState({ userName:'', is_teacher:'', password:'', email:'', phone:'', location:'' });
     });
   }
-  componentDidMount() {}
+  
 
   searchInfo(e) {
     console.log(this.state[e.target.name])
     e.preventDefault()
     this.setState({ [e.target.name]: e.target.value })
   }
-  componentDidMount() { }
+  
 
   onRatingChange(e) {
     this.setState({
@@ -147,7 +127,7 @@ class App extends React.Component {
       .then(data => { this.setState({ teacherProfiles: data.data }); console.log(this.state.teacherProfiles) });
 
   }
-  componentDidMount() { }
+ 
 
   render() {
     var tech = this.state.teacherProfiles;
