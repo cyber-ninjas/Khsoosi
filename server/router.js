55578ccaa32c0279a878fcae8662fa68be0f4f0b
engaymanh <eng.aymanhariri@gmail.com>
2019-05-20 22:27:18 +0300

    backup

--- server/router.js
+++ server/router.js
@@ -1,11 +1,10 @@
-
 var khsoosiRouter = require("express").Router();
 var controller = require("./controller");
 
 khsoosiRouter.route("/signup").post(controller.signup);
-khsoosiRouter.route("/search").get(controller.search);
+khsoosiRouter.route("/rating").post(controller.rating);
 khsoosiRouter.route("/login").get(controller.login);
+khsoosiRouter.route("/search").get(controller.search);
 khsoosiRouter.route("/classes").get(controller.seeSchedule);
 
 module.exports = khsoosiRouter;
-  
--- server/server.js
+++ server/server.js
@@ -1,35 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const formData = require('express-form-data');
 const db = require("./database/db");
-const {User, Schedule, Role, Permission, Subject, Rating, PermissionRole, UserRole, TeacherSubject} = require("./database/model");
-
 const app = express();
-const {search, rating, seeSchedule} = require('./controller')
-const khsoosiRouter = require('./router');
-
-
+const khsoosiRouter = require("./router");
 const port = process.env.PORT || 4000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
-app.use("",khsoosiRouter);
-app.use(express.static(__dirname + '/../client/dist'));
-
-app.post('/rating',(req,res)=>{
-  rating(req, res);
-})
+app.use("", khsoosiRouter);
+app.use(express.static(__dirname + "/../client/dist"));
 
 app.listen(port, function() {
-  console.log('listening on port !',port);
+  console.log("listening on port !", port);
 });
