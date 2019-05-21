015ccb9fba34ca0d110bd7a9d0bbddf420061548
Ahmad-Al-Darkashli <alderkshli.ahmad@gmail.com>
2019-05-21 12:46:51 +0300

    do showTecherInfo fetch in App

--- client/src/components/teacherProfile.jsx
+++ client/src/components/teacherProfile.jsx
@@ -13,12 +13,12 @@ class TeacherProfile extends React.Component {
     return (
       <div >
 
-			  <img src={this.props.imgUrl || 'https://via.placeholder.com/100x100'} alt = 'uploaded images' height='100' width='100' /><br/>
-				<a href = {this.props.cvFileUrl || "javascript:alert('No file exist');" } > CV </a><br/>
+			  <img src={this.props.teacherInfo.imgUrl || 'https://via.placeholder.com/100x100'} alt = 'uploaded images' height='100' width='100' /><br/>
+				<a href = {this.props.teacherInfo.cvFileUrl || "javascript:alert('No file exist');" } > CV </a><br/>
 				
 					<p>Please select your class time:</p>
   				{/* <input type="radio" name="gender" value=""/> <br/>  */}
-					<button onClick={() => this.props.gitTeacherProfile()}>show</button>
+					<button onClick={this.props.gitTeacherProfile}>show</button>
 				
 
       </div>
