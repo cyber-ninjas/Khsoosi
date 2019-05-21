75e38035fd42fd7e51195c944d2c9b0d61cb912c
iman-elokour <elokour.iman@gmail.com>
2019-05-21 09:41:57 +0300

    adding to schedule

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
 
 class Schedule extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    const { ScheduleVariables } = this.props;
-    return (
-      <div>
-        <form >
-          <input type="checkbox" /> <label>Sunday</label> <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Monday</label>  <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Tuesday</label> <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Wednesday</label> <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Thursday</label> <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Friday</label> <input placeholder="from"/> <input placeholder="to"/> <br />
-          <input type="checkbox" /> <label>Saturday</label> <input placeholder="from"/> <input placeholder="to"/>
-        </form>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<br />
+				<br />
+				<form>
+					<select name="day" onChange={this.props.change.bind(this)}>
+						<option value="Sunday"> Sunday</option>
+						<option value="Monday"> Monday</option>
+						<option value="Tuesday"> Tuesday</option>
+						<option value="Wedensday">Wedensday </option>
+						<option value="Thursday">Thursday </option>
+						<option value="Friday">Friday </option>
+						<option value="Saturday">Saturday </option>
+					</select>
+					<input placeholder="from" name="startHour" onChange={this.props.change.bind(this)} />
+					<input placeholder="to" name="endHour" onChange={this.props.change.bind(this)} />
+					<button onClick={this.props.addSchedule.bind(this)}>Add</button>
+				</form>
+				<br />
+				{this.props.schedule.length > 0 ? (
+					<div>
+						<h3>Schedule</h3>
+						<table>
+							<tbody>
+								<tr>
+									<th>Day</th>
+									<th>Start</th>
+									<th>End</th>
+									<th>Cancel</th>
+								</tr>
+								{this.props.schedule.map((sch, index) => {
+									return (
+										<tr key={sch.day} >
+											<td>{sch.day}</td>
+											<td>{sch.startHour}</td>
+											<td>{sch.endHour}</td>
+											<td>
+												<button name={sch.day} onClick={this.props.removeSchedule.bind(this)}>remove</button>
+											</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</div>
+				) : null}
+			</div>
+		);
+	}
 }
 
 export default Schedule;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,122 +1,164 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SignUp from "./components/SignUp.jsx"
-import Rating from "./components/Rating.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignUp from './components/SignUp.jsx';
+import Rating from './components/Rating.jsx';
 import Search from './components/search.jsx';
 import ResultSearch from './components/resultSearch.jsx';
 import Header from './components/Header.jsx';
-import Schedule from "./components/Schedule.jsx";
-
+import Schedule from './components/Schedule.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: "",
-      cvFile: "",
-      img: "",
-      summary: "",
-      is_teacher: false,
-      password: "",
-      email: "",
-      phone: "",
-      location: "",
-      teacherProfiles: [],
-      current_teacherId: "",
-      current_studentId: "",
-      ratingText: "",
-      rate: "",
-      subjectName: "",
-      subjectLevel: "",
-      day: "",
-      startHour: "",
-      endHour: ""
-    };
-  }
-  onchangingSignUp(e){
-    this.setState({[e.target.name]:e.target.value});
-  }
-  onSignUp(){
-    console.log("signup");
-    const { userName, is_teacher, password, email, phone, location } = this.state;
-    const body = { userName, is_teacher, password, email, phone, location };
-    fetch('/signup', {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" }
-    }).then((response) => {
-      return response.json();
-    }).then((body) => {
-      console.log(body)
-      this.setState({ userName:'', is_teacher:'', password:'', email:'', phone:'', location:'' });
-    });
-  }
-  componentDidMount() {}
+	constructor(props) {
+		super(props);
+		this.state = {
+			userName: '',
+			cvFile: '',
+			img: '',
+			summary: '',
+			is_teacher: false,
+			password: '',
+			email: '',
+			phone: '',
+			location: '',
+			teacherProfiles: [],
+			current_teacherId: '',
+			current_studentId: '',
+			ratingText: '',
+			rate: '',
+			subjectName: '',
+			subjectLevel: '',
+			day: 'Sunday',
+			startHour: '',
+			endHour: '',
+			schedule: []
+		};
+	}
+	onchangingSignUp(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	onSignUp() {
+		console.log('signup');
+		const { userName, is_teacher, password, email, phone, location } = this.state;
+		const body = { userName, is_teacher, password, email, phone, location };
+		fetch('/signup', {
+			method: 'post',
+			body: JSON.stringify(body),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((body) => {
+				console.log(body);
+				this.setState({ userName: '', is_teacher: '', password: '', email: '', phone: '', location: '' });
+			});
+	}
+	componentDidMount() {}
 
-  searchInfo(e) {
-    console.log(this.state[e.target.name])
-    e.preventDefault()
-    this.setState({ [e.target.name]: e.target.value })
-  }
-  
-  componentDidMount() { }
+	searchInfo(e) {
+		console.log(this.state[e.target.name]);
+		e.preventDefault();
+		this.setState({ [e.target.name]: e.target.value });
+	}
 
-  onRatingChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+	change(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
 
-  rating() {
-    const body = { ratingText: this.state.ratingText, rate: this.state.rate };
-    fetch('/rating', {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" }
-    }).then((response) => {
-      return response.json();
-    }).then((body) => {
-      console.log(body)
-      this.setState({ ratingText: '', rate: '' });
-    });
-  }
+	addSchedule(e) {
+		e.preventDefault();
+		const { day, startHour, endHour } = this.state;
+		const temp = this.state.schedule;
+		this.setState({
+			schedule: [ ...temp, { day, startHour, endHour } ]
+		});
+		console.log(this.state.schedule);
+	}
 
-  searchTecher(e) {
-
-    e.preventDefault();
-
-    // Default options are marked with *
-    return fetch(`/search/?location=${this.state.location}&name=${this.state.subjectName}&level=${this.state.subjectLevel}`, {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        // 'Content-Type': 'application/json',
-        'Accept': 'application/json'
+	removeSchedule(e) {
+    let { schedule } = this.state;
+    schedule.forEach((element, index) => {
+      console.log(element.day, e.name)
+      if(element.day === e.name){
+        schedule.splice(index, 1);
       }
+    });
+    this.setState({
+      schedule: schedule
     })
-      .then(response => response = response.json())
-      .then(data => { this.setState({ teacherProfiles: data.data }); console.log(this.state.teacherProfiles) });
+		//console.log(e.target.id);
+	}
 
-  }
-  componentDidMount() { }
+	rating() {
+		const body = { ratingText: this.state.ratingText, rate: this.state.rate };
+		fetch('/rating', {
+			method: 'post',
+			body: JSON.stringify(body),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((body) => {
+				console.log(body);
+				this.setState({ ratingText: '', rate: '' });
+			});
+	}
 
-  render() {
-    var tech = this.state.teacherProfiles;
-    var { ratingText, rate, current_studentId, current_teacherId } = this.state;
-    var RatingVariables = { ratingText, rate, current_studentId, current_teacherId };
-    var { current_teacherId, day, startHour, endHour } = this.state;
-    var ScheduleVariables = { current_teacherId, day, startHour, endHour };
-    return (
-      <div>
-        <Header />
-        <img src='https://www.trentu.ca/english/sites/trentu.ca.english/files/styles/header_image/public/header_images/header_creative_writing2.jpg?itok=qqMcjzSZ'/>
-        <h1>Test by Cyber-Ninjas</h1>
-        <SignUp onchangingSignUp={this.onchangingSignUp.bind(this)} onSignUp={this.onSignUp.bind(this)} is_teacher={this.state.is_teacher}/>
-        <Search searchTecher={this.searchTecher.bind(this)} searchInfo={this.searchInfo.bind(this)} />
-        <ResultSearch resultOfSer={tech} />
-        <Schedule ScheduleVariables={ScheduleVariables}/>
-        <Rating RatingVariables={RatingVariables} onChange={event => this.onRatingChange(event)} onClick={event => this.rating(event)} />
-      </div>
-    );
-  }
+	searchTecher(e) {
+		e.preventDefault();
+
+		// Default options are marked with *
+		return fetch(
+			`/search/?location=${this.state.location}&name=${this.state.subjectName}&level=${this.state.subjectLevel}`,
+			{
+				method: 'GET', // *GET, POST, PUT, DELETE, etc.
+				headers: {
+					// 'Content-Type': 'application/json',
+					Accept: 'application/json'
+				}
+			}
+		)
+			.then((response) => (response = response.json()))
+			.then((data) => {
+				this.setState({ teacherProfiles: data.data });
+				console.log(this.state.teacherProfiles);
+			});
+	}
+	componentDidMount() {}
+
+	render() {
+		var tech = this.state.teacherProfiles;
+		var { ratingText, rate, current_studentId, current_teacherId } = this.state;
+		var RatingVariables = { ratingText, rate, current_studentId, current_teacherId };
+
+		return (
+			<div>
+				<Header />
+				<img src="https://www.trentu.ca/english/sites/trentu.ca.english/files/styles/header_image/public/header_images/header_creative_writing2.jpg?itok=qqMcjzSZ" />
+				<h1>Test by Cyber-Ninjas</h1>
+				<SignUp
+					onchangingSignUp={this.onchangingSignUp.bind(this)}
+					onSignUp={this.onSignUp.bind(this)}
+					is_teacher={this.state.is_teacher}
+				/>
+				<Search searchTecher={this.searchTecher.bind(this)} searchInfo={this.searchInfo.bind(this)} />
+				<ResultSearch resultOfSer={tech} />
+				<Schedule
+					schedule={this.state.schedule}
+					change={this.change.bind(this)}
+					addSchedule={this.addSchedule.bind(this)}
+					removeSchedule={this.removeSchedule.bind(this)}
+				/>
+				<Rating
+					RatingVariables={RatingVariables}
+					onChange={(event) => this.change(event)}
+					onClick={(event) => this.rating(event)}
+				/>
+			</div>
+		);
+	}
 }
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
