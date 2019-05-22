41677a2f29cb7eb009de950748960e073aad6a87
iman-elokour <elokour.iman@gmail.com>
2019-05-22 13:25:55 +0300

    css start

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,65 +1,41 @@
-import React from "react";
+import React from 'react';
 // import Login from "./login.jsx";
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class SignUp extends React.Component {
-  render() {
-    return (
-      <div class="form-control form-control-lg">
-        <h2>SignUp</h2>
-        <label htmlFor="student">student</label>
-        <input
-          type="radio"
-          name="is_teacher"
-          id="student"
-          value="false"
-          onChange={this.props.change.bind(this)}
-        />
-        <label htmlFor="taecher">teacher</label>
-        <input
-          type="radio"
-          name="is_teacher"
-          id="teacher"
-          value="true"
-          onChange={this.props.change.bind(this)}
-        />
-        <input
-          type="text"
-          name="userName"
-          placeholder="your name"
-          onChange={this.props.change.bind(this)}
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="example@gmaill.com"
-          onChange={this.props.change.bind(this)}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="*****"
-          onChange={this.props.change.bind(this)}
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="7777788888"
-          onChange={this.props.change.bind(this)}
-        />
-        <input
-          type="text"
-          name="location"
-          placeholder="Amman"
-          onChange={this.props.change.bind(this)}
-        />
-        <label id="error">{this.props.error}</label>
-        <input
-          type="button"
-          value="SignUp"
-          onClick={this.props.onSignUp.bind(this)}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="form-control form-control-lg">
+				<h2>SignUp</h2>
+				<label htmlFor="student">student</label>
+				<input
+					type="radio"
+					name="is_teacher"
+					id="student"
+					value="false"
+					onChange={this.props.change.bind(this)}
+				/>
+				<label htmlFor="taecher">teacher</label>
+				<input
+					type="radio"
+					name="is_teacher"
+					id="teacher"
+					value="true"
+					onChange={this.props.change.bind(this)}
+				/>
+				<input type="text" name="userName" placeholder="your name" onChange={this.props.change.bind(this)} />
+				<input
+					type="text"
+					name="email"
+					placeholder="example@gmaill.com"
+					onChange={this.props.change.bind(this)}
+				/>
+				<input type="text" name="password" placeholder="*****" onChange={this.props.change.bind(this)} />
+				<input type="text" name="phone" placeholder="7777788888" onChange={this.props.change.bind(this)} />
+				<input type="text" name="location" placeholder="Amman" onChange={this.props.change.bind(this)} />
+				<label id="error">{this.props.error}</label>
+				<input type="button" value="SignUp" onClick={this.props.onSignUp.bind(this)} />
+			</div>
+		);
+	}
 }
 export default SignUp;
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class Login extends React.Component {
 	constructor(props) {
@@ -24,33 +24,11 @@ class Login extends React.Component {
 					placeholder="*******"
 				/>
 				<button onClick={this.props.loging.bind(this)}> login</button>
+				<label>{this.props.loginMessage}</label>
+				<label id="error">{this.props.errorLogin}</label>
 			</div>
 		);
 	}
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>login</h1>
-        <input
-          name="email"
-          onChange={this.props.change.bind(this)}
-          placeholder="example@gmail.com"
-        />
-        <input
-          name="password"
-          type="password"
-          onChange={this.props.change.bind(this)}
-          placeholder="*******"
-        />
-        <button onClick={this.props.loging.bind(this)}> login</button>
-        <label>{this.props.loginMessage}</label>
-        <label id="error">{this.props.errorLogin}</label>
-      </div>
-    );
-  }
 }
 
 export default Login;
