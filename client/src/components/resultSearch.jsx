456ad81ca4244e7a2a83ca4060d2ac00d9a6317d
engaymanh <eng.aymanhariri@gmail.com>
2019-05-23 18:24:33 +0300

    style

--- client/src/components/resultSearch.jsx
+++ client/src/components/resultSearch.jsx
@@ -1,31 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ResultSearch extends Component {
-	render() {
-		return (
-			<div>
-				<br />
-				{console.log(this.props.resultOfSer)}
-				{this.props.resultOfSer.map(function(name, index) {
-					return (
-						<div className="card" name={name.id} key={index}>
-							<img src={name.img} style={{ width: 100 + '%' }} />
-							<h1>{name.name}</h1>
-							<p className="title">{name.summary}</p>
-							<p>{name.reatingText}</p>
-							<span>
-								{name.rate - Math.floor(name.rate) == 0 ? 'Rate:' : <i className="fa fa-star-half" />}
+  render() {
+    const that = this;
+    return (
+      <div>
+        <br />
+        {/* {console.log(this.props.resultOfSer)} */}
 
-								{Array(Math.floor(name.rate / 2)).fill(<i className="fa fa-star" />)}
-							</span>
+        {this.props.resultOfSer.map(function(name, index) {
+          return (
+            <div
+              className="card"
+              name={name.id}
+              key={index}
+              onClick={that.props.openModal.bind(this, name.id)}
+            >
+              <img src={name.img} style={{ width: 100 + "%" }} />
+              <h1>{name.name}</h1>
+              <p className="title">{name.summary}</p>
+              <p>{name.reatingText}</p>
+              <span>
+                {name.rate - Math.floor(name.rate) == 0 ? (
+                  "Rate:"
+                ) : (
+                  <i className="fa fa-star-half" />
+                )}
 
-							<br />
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
+                {Array(Math.floor(name.rate / 2)).fill(
+                  <i className="fa fa-star" />
+                )}
+              </span>
+
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default ResultSearch;
--- client/src/components/teacherProfile.jsx
+++ client/src/components/teacherProfile.jsx
@@ -1,125 +1,132 @@
-import React from 'react';
-import Modal from 'react-awesome-modal';
-import Rating from './Rating.jsx';
+import React from "react";
+import Modal from "react-awesome-modal";
+import Rating from "./Rating.jsx";
 class TeacherProfile extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			Rate: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      Rate: false
+    };
+  }
 
-	openModal(e) {
-		this.setState({
-			[e]: true
-		});
-	}
+  openModal(e) {
+    this.setState({
+      [e]: true
+    });
+  }
 
-	closeModal(e) {
-		this.setState({
-			[e]: false
-		});
-	}
-	componentWillMount() {
-		this.props.showTeacherInfo();
-	}
-	render() {
-		// .pick&rateBtn {
-		//   display: 'flex',
-		//   flexDirection: 'column',
-		//   alignItems: 'center',
-		//   justifyContent: 'center'
-		// };
-		// const schedules = this.props.teacherInfo.schedules;
-		var that = this;
-		return (
-			<div className="container-fluid">
-				<div className="row">
-					<div className="teacherInfo col-sm-4">
-						<legend>Teacher Info</legend>
-						<img
-							src={this.props.teacherInfo.imgUrl || 'https://via.placeholder.com/100x100'}
-							alt="uploaded images"
-							height="100"
-							width="100"
-						/>
-						<br />
-						<label htmlFor="">Name: </label> {this.props.teacherInfo.userName}
-						<br />
-						<label htmlFor="">email: </label> {this.props.teacherInfo.email}
-						<br />
-						<label htmlFor="">Phone: </label> {this.props.teacherInfo.phone}
-						<br />
-						<label htmlFor="">Location: </label> {this.props.teacherInfo.location}
-						<br />
-						<legend>Students feedback</legend>
-						<div className="ratingScroll">
-							{this.props.teacherInfo.ratings.map((rates, index) => {
-								return (
-									<div key={index}>
-										<label htmlFor="">Testimonial: </label> {rates.text} <br />
-										<label htmlFor="">Rating level: </label> {rates.rate} <br />
-									</div>
-								);
-							})}
-						</div>
-					</div>
+  closeModal(e) {
+    this.setState({
+      [e]: false
+    });
+  }
+  componentWillMount() {
+    this.props.showTeacherInfo();
+  }
+  render() {
+    // .pick&rateBtn {
+    //   display: 'flex',
+    //   flexDirection: 'column',
+    //   alignItems: 'center',
+    //   justifyContent: 'center'
+    // };
+    // const schedules = this.props.teacherInfo.schedules;
+    var that = this;
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="teacherInfo col-sm-4">
+            <legend>Teacher Info</legend>
+            <img
+              src={
+                this.props.teacherInfo.imgUrl ||
+                "https://via.placeholder.com/100x100"
+              }
+              alt="uploaded images"
+              height="200"
+              width="100"
+            />
+            <br />
+            <label htmlFor="">Name: </label> {this.props.teacherInfo.userName}
+            <br />
+            <label htmlFor="">email: </label> {this.props.teacherInfo.email}
+            <br />
+            <label htmlFor="">Phone: </label> {this.props.teacherInfo.phone}
+            <br />
+            <label htmlFor="">Location: </label>{" "}
+            {this.props.teacherInfo.location}
+            <br />
+            <legend>Students feedback</legend>
+            <div className="ratingScroll">
+              {this.props.teacherInfo.ratings.map((rates, index) => {
+                return (
+                  <div key={index}>
+                    <label htmlFor="">Testimonial: </label> {rates.text} <br />
+                    <label htmlFor="">Rating level: </label> {rates.rate} <br />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
 
-					<div className="teacherCV col-sm-4">
-						<legend>Summary</legend>
-						<label htmlFor="">Summary: </label> <p>{this.props.teacherInfo.summary}</p>
-						<legend>Teacher CV</legend>
-						<iframe src={this.props.teacherInfo.cvFileUrl}>
-							<p>{"javascript:alert('No file exist');"}</p>
-						</iframe>
-					</div>
+          <div className="teacherCV col-sm-4">
+            <legend>Summary</legend>
+            <label htmlFor="">Summary: </label>{" "}
+            <p>{this.props.teacherInfo.summary}</p>
+            <legend>Teacher CV</legend>
+            <iframe src={this.props.teacherInfo.cvFileUrl}>
+              <p>{"javascript:alert('No file exist');"}</p>
+            </iframe>
+          </div>
 
-					<div className="teacherSchedule col-sm-4">
-						<legend>Teacher Schedule</legend>
-						<ul>
-							<p>Please select your class time:</p>
-							{this.props.teacherInfo.schedules.map((time, index) => {
-								return (
-									<li key={index}>
-										{' '}
-										<input
-											type="radio"
-											name="day"
-											value={`${time.day} ${time.startHour} ${time.endHour}`}
-											onClick={this.props.radioChange.bind(this)}
-											id={`radio${index}`}
-										/>{' '}
-										{time.day}
-										{' start at:'} {time.startHour} {' end at:'} {time.endHour}
-									</li>
-								);
-							})}
-						</ul>
-						<button onClick={this.props.pick.bind(this)}>Pick</button>
-						<label id="pickLabel">your request was send ...wait for confirm </label>
-						<br />
-						<button onClick={() => this.openModal('Rate')}>Rate</button>
-						<Modal
-							visible={this.state.Rate}
-							width="400"
-							height="300"
-							effect="fadeInDown"
-							onClickAway={() => this.closeModal('Rate')}
-						>
-							<div>
-								<Rating
-									rateMessage={this.props.rateMessage}
-									RatingVariables={this.props.RatingVariables}
-									change={this.props.change.bind(this)}
-									rating={this.props.rating.bind(this)}
-								/>
-							</div>
-						</Modal>
-					</div>
-				</div>
-			</div>
-		);
-	}
+          <div className="teacherSchedule col-sm-4">
+            <legend>Teacher Schedule</legend>
+            <ul>
+              <p>Please select your class time:</p>
+              {this.props.teacherInfo.schedules.map((time, index) => {
+                return (
+                  <li key={index}>
+                    {" "}
+                    <input
+                      type="radio"
+                      name="day"
+                      value={`${time.day} ${time.startHour} ${time.endHour}`}
+                      onClick={this.props.radioChange.bind(this)}
+                      id={`radio${index}`}
+                    />{" "}
+                    {time.day}
+                    {" start at:"} {time.startHour} {" end at:"} {time.endHour}
+                  </li>
+                );
+              })}
+            </ul>
+            <button onClick={this.props.pick.bind(this)}>Pick</button>
+            <label id="pickLabel">
+              your request was send ...wait for confirm{" "}
+            </label>
+            <br />
+            <button onClick={() => this.openModal("Rate")}>Rate</button>
+            <Modal
+              visible={this.state.Rate}
+              width="400"
+              height="300"
+              effect="fadeInDown"
+              onClickAway={() => this.closeModal("Rate")}
+            >
+              <div>
+                <Rating
+                  rateMessage={this.props.rateMessage}
+                  RatingVariables={this.props.RatingVariables}
+                  change={this.props.change.bind(this)}
+                  rating={this.props.rating.bind(this)}
+                />
+              </div>
+            </Modal>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TeacherProfile;
