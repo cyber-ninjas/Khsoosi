169d97deee0a13e6e483114b20dc78a1fe45cc94
Ahmad-Al-Darkashli <alderkshli.ahmad@gmail.com>
2019-05-23 14:47:36 +0300

    cvUpload & imageUpload styling

--- client/src/components/cvUpload.jsx
+++ client/src/components/cvUpload.jsx
@@ -3,32 +3,38 @@ import React from 'react';
 class CVUpload extends React.Component {
 	render() {
 		return (
-			<div className="cvDiv input-group">
-				<div className="input-group-prepend">
-					<button
-						className="btn btn-primary"
-						type="button"
-						id="inputGroupFileAddon01"
-						onClick={this.props.handleFileUpload.bind(this)}
-					>
-						Upload
-					</button>
+			<div className="cvDiv">
+				<div className="input-group">
+					<div className="input-group-prepend">
+						<button
+							className="uploadBtn btn btn-outline-primary btn-sm"
+							type="button"
+							id="inputGroupFileAddon01"
+							onClick={this.props.handleFileUpload.bind(this)}
+						>
+							Upload
+						</button>
+					</div>
+					<div className="custom-file">
+						<input
+							className="custom-file-input"
+							id="inputGroupFile01"
+							aria-describedby="inputGroupFileAddon01"
+							type="file"
+							accept=".doc,.docx,.txt,.pdf"
+							data-max-size="5000"
+							onChange={this.props.handleFileChange.bind(this)}
+						/>
+						<label className="custom-file-label" htmlFor="inputGroupFile01">
+							Choose file
+						</label>
+					</div>
 				</div>
-				<div className="custom-file">
-					<input
-						className="custom-file-input"
-						id="inputGroupFile01"
-						aria-describedby="inputGroupFileAddon01"
-						type="file"
-						accept=".doc,.docx,.txt,.pdf"
-						data-max-size="5000"
-						onChange={this.props.handleFileChange.bind(this)}
-					/>
-					<label className="custom-file-label" htmlFor="inputGroupFile01">
-						Choose file
-					</label>
+				<div className="row">
+					<div className="col-sm-12">
+						<progress className="progBar" value={this.props.cvProgress} max="100" />
+					</div>
 				</div>
-				<progress value={this.props.cvProgress} max="100" />
 			</div>
 		);
 	}
--- client/src/components/imageUpload.jsx
+++ client/src/components/imageUpload.jsx
@@ -11,24 +11,47 @@ class ImageUpload extends React.Component {
 		};
 
 		return (
-			<div className="imgDiv input-group">
-				<progress value={this.props.imageProgress} max="100" />
-				<br />
-				<input
-					type="file"
-					accept="image/*"
-					data-max-size="5000"
-					onChange={this.props.handleImgChange.bind(this)}
-				/>
-
-				<br />
-				<img
-					src={this.props.imgUrl || 'https://via.placeholder.com/100x100'}
-					alt="uploaded images"
-					height="100"
-					width="100"
-				/>
-				<button onClick={this.props.handleImgUpload.bind(this)}>Upload Your Image Here</button>
+			<div>
+				<div className="input-group mb-3">
+					<div className="input-group-prepend">
+						<button
+							className="uploadBtn btn btn-outline-primary btn-sm"
+							type="button"
+							id="inputGroupFileAddon01"
+							onClick={this.props.handleImgUpload.bind(this)}
+						>
+							Upload
+						</button>
+					</div>
+					<div className="custom-file">
+						<input
+							className="custom-file-input"
+							id="inputGroupFile01"
+							aria-describedby="inputGroupFileAddon01"
+							type="file"
+							accept="image/*"
+							data-max-size="5000"
+							onChange={this.props.handleImgChange.bind(this)}
+						/>
+						<label className="custom-file-label" htmlFor="inputGroupFile01" value="">
+							Choose file
+						</label>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-4">
+						<img
+							src={this.props.imgUrl || 'https://via.placeholder.com/100x100'}
+							alt="uploaded images"
+							height="100"
+							width="100"
+						/>
+					</div>
+					<div className="col-sm-4">
+						<progress value={this.props.imageProgress} max="100" />
+					</div>
+					<div className="col-sm-4" />
+				</div>
 			</div>
 		);
 	}
