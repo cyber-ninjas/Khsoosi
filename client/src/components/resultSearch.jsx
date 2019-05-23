e62c0efc095e36471e58a00210e518e5ff92d0c7
iman-elokour <elokour.iman@gmail.com>
2019-05-23 13:34:40 +0300

    css search results and add not found msg

--- client/src/components/resultSearch.jsx
+++ client/src/components/resultSearch.jsx
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class ResultSearch extends Component {
-  render() {
-    return (
-      <div>
-        <br />
-        {this.props.resultOfSer.map(function(name, index) {
-          // console.log("hello");
-          return (
-            <div className="card" name={name.id} key={index}>
-              <img src={name.img} style={{ width: 100 + "%" }} />
-              <h1>{name.name}</h1>
-              <p className="title">{name.summary}</p>
-              <p>{name.reatingText}</p>
-              <div style={{ margin: 24 + "px" + 0 }}>{name.rate}/10</div>
-              <br />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<br />
+				{this.props.resultOfSer.length > 0 ? (
+					this.props.resultOfSer.map(function(name, index) {
+						return (
+							<div className="card" name={name.id} key={index}>
+								<img src={name.img} style={{ width: 100 + '%' }} />
+								<h2 className="cardtext">{name.name}</h2>
+								<h3 className="cardtext">{name.summary}</h3>
+								<h3 className="cardtext">{name.reatingText}</h3>
+								<div className="cardtext">{name.rate}/10</div>
+								<br />
+							</div>
+						);
+					})
+				) : (
+					<h1 id="notfound">Sorry, we could not find a match for your search</h1>
+				)}
+			</div>
+		);
+	}
 }
 
 export default ResultSearch;
